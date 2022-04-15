f24bc344157b2c2144dc014e868686d98cb8e5d4
Eric Schmidt <erschmid@google.com>
2022-04-15 10:26:19 -0700

    feat: kata3 simple web server

--- kata3/kata3_test.go
+++ kata3/kata3_test.go
@@ -1,3 +1,28 @@
-package kata3
+package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error(err)
+	}
+	want := "URL"
+	s := string(data)
+	if !strings.Contains(s, want) {
+		t.Errorf("wanted %s; got %s\n", want, s)
+	}
+}
