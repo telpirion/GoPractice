f8089586f19582346c45c0d0d1cb035eedab0ac7
Eric Schmidt <erschmid@cloudadvocacyorg.joonix.net>
2022-05-23 20:27:36 -0700

    feat: more work on linkedlist

--- kata5/kata5.go
+++ kata5/kata5.go
@@ -22,14 +22,55 @@ func (l *LinkedList) Add(d interface{}) {
 		l.Head = &ln
 	}
 
-	tail := iterateToEnd(*l.Head)
+	tail := iterateToEnd(l.Head, nil)
 	tail.Next = &ln
 }
 
-func iterateToEnd(n LinkedListNode) *LinkedListNode {
-	if n.Next == nil {
-		return &n
+func (l *LinkedList) Remove(val interface{}) *LinkedListNode {
+	if l.Head == nil {
+		return nil
 	}
 
-	return iterateToEnd(*n.Next)
+	n := iterateToValue(l.Head, val)
+	next := n.Next
+	n.Next = next.Next
+	return next
+}
+
+func (l *LinkedList) Print() {
+
+	if l.Head == nil {
+		return
+	}
+
+	print := func(n *LinkedListNode) {
+		fmt.Println(n.Datum)
+	}
+
+	iterateToEnd(l.Head, &print)
+}
+
+func iterateToEnd(n *LinkedListNode, f *func(*LinkedListNode)) *LinkedListNode {
+
+	if f != nil {
+		(*f)(n)
+	}
+
+	if n.Next == nil {
+		return n
+	}
+
+	return iterateToEnd(n.Next, f)
+}
+
+func iterateToValue(n *LinkedListNode, val interface{}) *LinkedListNode {
+	if n.Next == nil {
+		return nil
+	}
+
+	if n.Next.Datum == val {
+		return n
+	}
+
+	return iterateToValue(n.Next, val)
 }
